88dea8c963445fc60d7cbaecf95d78416567dfcb
Cade Weinberg <cadeweinberg@gmail.com>
2026-04-02 12:50:22 -0700

    getting boost to integrate with all targets

--- cmake/findDependencies.cmake
+++ cmake/findDependencies.cmake
@@ -25,52 +25,34 @@
 
 cmake_minimum_required (VERSION 3.28)
 
-find_package(Boost CONFIG 
-    COMPONENTS
-    unit_test_framework
-    program_options
+include(FetchContent)
+
+set(BOOST_INCLUDE_LIBRARIES program_options test leaf random)
+set(BOOST_ENABLE_CMAKE ON)
+
+FetchContent_Declare(
+    Boost
+    URL https://github.com/boostorg/boost/releases/download/boost-1.90.0/boost-1.90.0-cmake.zip
+    URL_HASH SHA256=89dc8dbdd61b0212dfff5286a07227cc40d0243f344037739b7c7f7f4a6c3584
 )
 
-if (Boost_FOUND)
-    message(STATUS "Boost found: ${Boost_INCLUDE_DIRS}")
-else ()
-    message(STATUS "Boost not found, fetching from GitHub...")
-    include(FetchContent)
-    FetchContent_Declare(
-        Boost
-        GIT_REPOSITORY https://github.com/boostorg/boost.git
-        GIT_TAG 1bed2b0712b2119f20d66c5053def9173c8462a5
-    )
+#set(BOOST_INCLUDE_LIBRARIES program_options test CACHE STRING "")
 
-    # Configure Boost to only build what we need
-    set(BOOST_INCLUDE_LIBRARIES program_options test CACHE STRING "")
-    set(BOOST_ENABLE_CMAKE ON CACHE BOOL "")
+#FetchContent_Declare(
+#    re2c
+#    GIT_REPOSITORY https://github.com/skvadrik/re2c.git
+#    GIT_TAG 8a44906f5c5df26138d2202c335bf9a9f5f2fefe
+#)
 
-    # Enable __declspec attribute support for Emscripten
-    if(EMSCRIPTEN)
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fms-extensions" CACHE STRING "")
-    endif()
+FetchContent_MakeAvailable(Boost)
 
-    FetchContent_MakeAvailable(Boost)
-    # I don't understand why this is necessary, given that the headers target includes 
-    # leaf when we are using the system level install.
-    if(TARGET Boost::headers AND NOT TARGET Boost::headers_with_leaf)
-        add_library(boost_headers_with_leaf INTERFACE)
-        add_library(Boost::headers_with_leaf ALIAS boost_headers_with_leaf)
-        target_link_libraries(boost_headers_with_leaf INTERFACE Boost::headers)
-        target_include_directories(boost_headers_with_leaf INTERFACE 
-            "${boost_SOURCE_DIR}/libs/leaf/include"
-            "${boost_SOURCE_DIR}/libs/random/include"
-            "${boost_SOURCE_DIR}/libs/integer/include"
-        )
-    endif()
-endif ()
+#message(STATUS "re2c available ${re2c_BINARY_DIR}")
 
 find_program(RE2C re2c REQUIRED
     DOC "The generator program for the Lexer"
 )
 
-if (RE2C_FOUND)
+if (re2c_FOUND)
     message(STATUS "re2c found: ${RE2C}")
 endif ()
 
